2ef96efc757a395f3cdddd9d57483efb492d0a82
Goodness Ezeokafor <gootech442@gmail.com>
2021-06-30 21:22:54 +0100

    created API class

--- package/Api.py
+++ package/Api.py
@@ -1,9 +1,66 @@
+from config import STAGING_BASE_URL,PRODUCTION_BASE_URL,SDK_ENVIRONMENTS
 import httpx
+import trio
+import os
+
+# open connection 
+
 class Api:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, token, environment,url='/rate') -> None:
+        if not token: raise TypeError("please enter a valid token")
+        if environment not in SDK_ENVIRONMENTS: raise TypeError("please enter a valid environment")
+        if not url : raise TypeError("please enter a valid url")
+        if url[0] != "/": url = "/" + url
+        self._token = token  # set auth token
+        self._headers = {
+            'token':self._token,
+            'Content-Type':'application/json'
+        } # set headers
+        self._url = url # set url
+        self._base_url = PRODUCTION_BASE_URL if environment == "production" or environment == "prod" else STAGING_BASE_URL
     
-    def test_works(self):
-        print("class works")
+    def health(self):
+        return "works"
 
+    async def get(self):
+        '''
+        function for making GET requests
+        returns the data
+        '''
+        response = ''
+        output_response = {}
+        async with httpx.AsyncClient(headers=self._headers,base_url=self._base_url) as client:
+            response = await client.get(self._url)
+        # return response.json()
+        new_responses_json = response.json()
+        for resp_keys,resp_values in new_responses_json.items():
+             # print(resp_keys, ":", resp_values)
+            output_response[resp_keys] = resp_values
+        # print("new oh",output_response['message'])
+        return output_response
 
+    async def post(self):
+        return "POST REQUEST"
+
+    def get_base_url(self):
+        return self._base_url
+    def get_url(self):
+        return self._url
+
+if __name__ == "__main__":
+    api = Api('3ee3701e057b26c6b55d0bee2', "dev", "webhook")
+    print(api.health())
+    print(api.get_base_url())
+    print(api.get_url())    
+    run_get = trio.run(api.get)
+    # run_post = trio.run(api.post)
+    # # trio.run(print(api.post))
+    print(run_get['code'])
+    # resp_dict={}
+    # # resp_dict -
+    # for resp_keys,resp_values in run_get.items():
+    #     # print(resp_keys, ":", resp_values)
+    #     resp_dict[resp_keys] = resp_values
+    # print("----------new dictionary-------------")
+    # print("new oh",resp_dict['message'])
+    # print("----------new dictionary-------------")
